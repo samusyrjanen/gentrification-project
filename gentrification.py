6b3be6af7d82eaf9ca682a7483cb1d74a9a3db23
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-09-21 13:36:35 +0300

    some initial thinking

--- gentrification.py
+++ gentrification.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Sample data for yearly income changes (replace this with your actual data)
-income_changes = [5, 8, 2, -3, -1, 6, 4, 9, 7, -2]
+# Sample data for yearly income of a district (replace this with your actual data)
+income = [5, 8, 2, 3, 1, 6, 4, 9, 7, 2]
 
-# Calculate the gentrification index
-gentrification_index = [(income_changes[i] - income_changes[i - 1]) / income_changes[i - 1] * 100 for i in range(1, len(income_changes))]
+# Calculate the income change of the district
+change = [income[i] - income[i - 1] for i in range(1, len(income))]
+print(f'Change from the previous value:\n{change}')
 
-# Normalize the index
-min_index = min(gentrification_index)
-max_index = max(gentrification_index)
-normalized_index = [2 * ((x - min_index) / (max_index - min_index)) - 1 for x in gentrification_index]
+# Calculate the mean change (in the future this would calculate the mean change of all districts that year)
+mean_change = sum(change) / len(change)
+print(f'\nMean change per year: {mean_change}')
 
-# Apply the sigmoid function
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+# Difference to the mean change
+difference_to_mean = [income_change - mean_change for income_change in change]
+print(f'\nDifference to the mean change:\n{difference_to_mean}')
 
-final_distribution = [sigmoid(x) for x in normalized_index]
+# Normalize the difference to the mean to a scale from -1 to 1
+max_difference = max(difference_to_mean, key=abs)
+normalized_difference = [difference / abs(max_difference) for difference in difference_to_mean]
+print(f'\nNormalized Difference to Mean (-1 to 1):\n{normalized_difference}')
 
 # Plot the results
-plt.plot(final_distribution)
+plt.plot(difference_to_mean)
 plt.xlabel('Year')
 plt.ylabel('Gentrification Score')
 plt.title('Gentrification Score Over Time')
